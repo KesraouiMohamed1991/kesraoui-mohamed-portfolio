71b8b545547d7136624c9cb3105bdb95d4f59ca1
KesraouiMohamed1991 <kesraoui15003@gmail.com>
2025-09-23 21:16:15 +0200

    updating lenis legacy

--- components/lenis-provider.tsx
+++ components/lenis-provider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { createContext, useContext, useEffect, useRef, useState } from 'react'
-import Lenis from '@studio-freight/lenis'
+import Lenis from 'lenis'
 
 const LenisContext = createContext<Lenis | null>(null)
 
